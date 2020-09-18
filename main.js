9c3c3822a841fc0e1f91a4596c15553f255565cf
ADRIÁN FRANCISCO MUÑOZ FELDER <admuofel@upv.edu.es>
2020-09-18 21:49:32 +0200

    added event bus and tabs (still not working)

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+var eventBus = new Vue()
+
+Vue.component('product-tabs', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+    <div>
+        <span class="tab"
+        :class="{ activeTab: selectedTab === tab}"
+        v-for="(tab, index) in tabs" :key="index"
+        @click="selectedTab = tab">
+        {{ tab }}
+        </span>
+
+        <div v-show="selectedTab === 'Reviews'">
+            <p v-if="!reviews.length">There are no reviews yet</p>
+                <li v-for="(review, index) in reviews" :key="index">
+                    <p>{{ review.name }}</p>
+                    <p>Rating: {{ review.rating }}</p>
+                    <p>{{ review.review }}</p>
+                </li>
+            </ul>
+        </div>
+
+        <product-review v-show="selectedTab === 'Make a Review'">>
+        </product-review>
+
+    </div>
+    `,
+    data() {
+        return{
+            tabs: ['Review', 'Make a Review'],
+            selectedTab: 'Reviews'
+        }
+    }
+})
+
 Vue.component('product-review', {
     template:`
     <form class="review-form" @submit.prevent="onSubmit">
@@ -52,7 +93,7 @@ Vue.component('product-review', {
                     review: this.review,
                     rating: this.rating
                 }
-                this.$emit('review-submitted', productReview)
+                eventBus.$emit('review-submitted', productReview)
                 this.name = null,
                 this.review = null,
                 this.rating = null
@@ -83,11 +124,6 @@ Vue.component('product', {
                     <li v-for="detail in details">{{ detail }}</li>
                 </ul>
 
-                <button @click="addToCart" 
-                :disabled="inStock === 0"
-                :class="{ disabledButton: inStock === 0 }">Add to Cart</button>
-                <button v-on:click="emptyCart">Empty Cart</button>
-
                 <div v-for="(variant, index) in variants" 
                 :key="variant.variantId"
                 class="color-box"
@@ -95,21 +131,14 @@ Vue.component('product', {
                 @mouseover="updateProduct(index)">
                 </div>
 
+                <button @click="addToCart" 
+                :disabled="inStock === 0"
+                :class="{ disabledButton: inStock === 0 }">Add to Cart</button>
+                <button v-on:click="emptyCart">Empty Cart</button>
+
             </div>
 
-            <div>
-                <h2>Reviews</h2>
-                <p v-if="!reviews.length">There are no reviews yet</p>
-                <ul>
-                    <li v-for="review in reviews">
-                        <p>{{ review.name }}</p>
-                        <p>Rating: {{ review.rating }}</p>
-                        <p>{{ review.review }}</p>
-                    </li>
-                </ul>
-            </div>
-
-            <product-review @review-submitted="addReview"></product-review>
+            <product-tabs :reviews="reviews"></product-tabs>
 
         </div>
     `,
@@ -147,9 +176,6 @@ Vue.component('product', {
         emptyCart() {
             this.$emit('empty-cart')
         },
-        addReview(productReview) {
-            this.reviews.push(productReview)
-        }
     },
     computed: {
         title() {
@@ -161,8 +187,12 @@ Vue.component('product', {
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
         }
+    },
+    mounted() {
+        eventBus.$on('review-submitted', productReview => {
+            this.reviews.push(productReview)
+        })
     }
-
 })
 
 var app = new Vue({
